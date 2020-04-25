094597ee8de5039c80c7b31b68878bd6b6fe82d4
Kim <kthkim@vlsi24.bu.edu>
2020-04-25 19:52:51 -0400

    added getArtist function

--- parsing/parsingMain.java
+++ parsing/parsingMain.java
@@ -15,13 +15,15 @@ public class parsingMain{
 	private static int date;
 	private static ArrayList<Integer> playcountArray = new ArrayList<Integer>();
 	private static ArrayList<Integer> dateArray = new ArrayList<Integer>();
+	private static String text;
+	private static int songIndex;
 
 	public static void main(String[] args){
 		String filename = "GoodFaith.txt";
 		String songName = "Nirvana";
-		String artist = "Madeon";
 		System.out.println(getPlaycount(filename, songName));
 		System.out.println(date);
+		System.out.println(getArtist(filename, songName));
 		//writeTextFile(songName, artist);
 	}
 
@@ -35,7 +37,6 @@ public class parsingMain{
 
 	//Returns playcount from txt file
 	public static int getPlaycount(String filename, String songName){
-		String text = "";
 		try{
 			text = new String(Files.readAllBytes(Paths.get(filename)));
 		}
@@ -43,7 +44,7 @@ public class parsingMain{
 			e.printStackTrace();
 		}
 		try{
-			int songIndex = text.indexOf(songName);
+			songIndex = text.indexOf(songName);
 			//9 characters between play count and song name
 			int lastDigitIndex = songIndex - 9;
 			//Assuming playcount < 100bil, playcount should be between firstIndexGuess and lastDigitIndex
@@ -67,16 +68,26 @@ public class parsingMain{
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 		String currentDate  = LocalDate.now().format(formatter);
 		date = Integer.parseInt(currentDate);
+		
+		playcountArray.add(playcount);
+
+		dateArray.add(date);
 
 		return playcount;
 	}
 
-	public static void appendPlaycountArray(int newPlaycount){
-		playcountArray.add(newPlaycount);
-	}
-
-	public static void appendDateArray(int newDate){
-		dateArray.add(newDate);
+	public static String getArtist(String filename, String songName){
+		String artist = "";
+		int nameIndex;
+		String subText = text.substring(songIndex);
+		nameIndex = subText.indexOf("artists");
+		int artistIndex = nameIndex + 19;
+		int i = artistIndex;
+		while(subText.charAt(i) != '\"'){
+			artist = artist + Character.toString(subText.charAt(i));
+			i++;
+		}
+		return artist;
 	}
 	
 /*	public static void writeTextFile(String songName, String artist){
