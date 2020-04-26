62afa3e2d38d39fa37db17424d7751b20ec39878
ebnb-eugeniobena <60407920+ebnb-eugeniobena@users.noreply.github.com>
2020-04-26 16:10:13 -0500

    fix 1

--- src/index.js
+++ src/index.js
@@ -1,50 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const app = express();
-app.use(bodyParser.json({ 
-    extended: false,
-    type:["application/json", "text/plain"]
- }));
-//app.use(pino);
-
-const SpotifyHelper= require("./api/spotifyHelper")
-
-const spotifyHelper= new SpotifyHelper()
-
-const FileReader= require("./api/filereader")
-
-const filereader= new FileReader()
-
-app.post('/api/readFile', (req, res) => {
-    console.log("readfile called")
-    res.setHeader('Content-Type', 'application/json');
-    filereader.readFile("./server/playcountdata.txt", (data)=>{
-        res.send(JSON.stringify({ success: true, data:data }));
-    })
-    
-});
-
-app.post('/api/test', (req, res) => {
-    console.log("api test recieved")
-    const body = req.body;
-    console.log(body);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ success: true, data:body }));
-});
-
-app.post('/api/getAlbums', (req, res) => {
-    console.log("api getAlbums recieved")
-    const body = req.body;
-    console.log(body);
-    res.setHeader('Content-Type', 'application/json');
-    spotifyHelper.getAlbums(body.artistId, (data)=>{
-        res.send(JSON.stringify({ success: true, data:data }));
-    })
-    
-});
-
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
